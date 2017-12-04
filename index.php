1d25ab45016e2579c073d29fb3593e3bd1c15ded
Bart <mac@macs-MacBook-Pro.local>
2017-12-04 23:05:36 +0000

    test 23

--- index.php
+++ index.php
@@ -30,7 +30,7 @@ require_once ('./inc/mostCommonWords.php');
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
         </button>
-        <a class="navbar-brand" href="index.php">News Authenticator</a>
+        <a class="navbar-brand" href="index.php">News Authenticator 2</a>
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
